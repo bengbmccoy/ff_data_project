f271cb13950d2c2076e399247a037b915018b76a
bengbmccoy <benjamingbmccoy@gmail.com>
2025-09-25 13:37:29 +1000

    simpllified this test and will move the play type unpacking into another layer. Now I have a table of each event by event type for each game, but the event type data is messy and needs more finesse to unpack

--- code/transform/transform_play_by_play.py
+++ code/transform/transform_play_by_play.py
@@ -9,6 +9,34 @@ import pandas as pd
 import sqlite3
 import sys
 
+def flatten_json(nested_json, parent_key='', sep='_'):
+    """
+    Flattens a nested JSON object into a single-level dictionary.
+
+    Args:
+        nested_json (dict): The nested JSON object to flatten.
+        parent_key (str): The prefix for keys in the flattened dictionary.
+        sep (str): The separator to use between parent and child keys.
+
+    Returns:
+        dict: A flattened dictionary.
+    """
+    flat_dict = {}
+    for key, value in nested_json.items():
+        new_key = f"{parent_key}{sep}{key}" if parent_key else key
+        if isinstance(value, dict):
+            flat_dict.update(flatten_json(value, new_key, sep=sep))
+        elif isinstance(value, list):
+            for i, item in enumerate(value):
+                if isinstance(item, dict):
+                    flat_dict.update(flatten_json(item, f"{new_key}{sep}{i}", sep=sep))
+                else:
+                    flat_dict[f"{new_key}{sep}{i}"] = item
+        else:
+            flat_dict[new_key] = value
+    return flat_dict
+
+
 # parse arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('-write', action='store_true', default=False,
@@ -27,27 +55,22 @@ if args.verbose:
 
 json_data = []
 
+paths = []
+values = []
+
+event_elements = []
+
 for i in json_files:
     with open(os.path.join(path_to_json, i)) as f:
+
         json_data.append(json.load(f))
 
-# init the rush play dataframe
-rush_result_columns = ['game_id', 'game_type', 'game_weather', 'year', 'season_type', 'week', 'period_id', 'period_num', 
-                    'pbp_id', 'pbp_type', 'event_id', 'event_type', 'clock', 
-                  'home_points', 'away_points', 'play_type', 'play_description', 'men_in_box', 'fake_punt', 
-                  'fake_field_goal', 'screen_pass', 'blitz', 'play_direction', 'left_tightends', 'right_tightends',
-                  'hash_mark', 'qb_at_snap', 'huddle', 'running_lane', 'play_action', 'run_pass_option', 'official',
-                  'start_clock', 'start_down', 'start_yfd', 'start_possession', 'start_location_team', 'start_location_yardline',
-                  'end_clock', 'end_down', 'end_yfd', 'end_possession', 'end_location_team', 'end_location_yardline',
-                  'attempt', 'yards', 'firstdown', 'inside_20', 'goal_to_go', 'broken_tackles', 'kneel_down', 'scramble', 
-                  'yards_after_contact', 'player_id', 'player_name', 'player_position', 'result']
-results_df = pd.DataFrame(columns=rush_result_columns)
-
 # init the pass play dataframe
-pass_result_columns = ['game_id', 'period_id', 'pbp_id', 'pbp_type', 'event_id', 'event_type', 'clock', 
-                  'home_points', 'away_points', 'play_type', 'play_description', 'fake_punt', 'fake_field_goal', 
-                  'screen_pass', 'play_action', 'run_pass_option']
-results_df = pd.DataFrame(columns=pass_result_columns)
+play_data_columns = ['game_id', 'game_type', 'weather', 'year', 'season_type', 'week', 
+                     'period_type', 'period_id', 'period_number', 
+                     'event_id', 'play_type', 'clock', 'home_points', 'away_points', 'description', 'flat_events_dict']
+results_df = pd.DataFrame(columns=play_data_columns)
+
 
 
 # iterate through JSONs
@@ -78,72 +101,57 @@ for data in json_data:
 
             if play['type'] == "drive":
 
-                for event in play['events']:
+                for i in play['events']:
 
-                    if event['type'] == 'play':
+                    try:
 
-                        row_event_id = event['id']
-                        row_play_type = event['play_type']
-                        row_play_clock = event['clock']
+                        row_event_id = i['id']
+                        row_play_type = i['play_type']
+                        row_clock = i['clock']
+                        row_home_points = i['home_points']
+                        row_away_points = i['away_points']
+                        row_play_description = i['description']
 
-                        if event['play_type'] == 'rush':
-                            print('RUSH')
-                        elif event['play_type'] == 'pass':
-                            print('PASS')
-                        elif event['play_type'] == 'punt':
-                            print('PUNT')
-                        elif event['play_type'] == 'field_goal':
-                            print('FIELD GOAL')
-                        elif event['play_type'] == 'kickoff':
-                            print('KICKOFF')
-                        elif event['play_type'] == 'extra_point':
-                            print('XP')
-                        elif event['play_type'] == 'penalty':
-                            print('PENALTY')
-                        elif event['play_type'] == 'conversion':
-                            print('CONVERSION')
-                        else:
-                            print('##########################')
-                            print(event['play_type'])
-                    
+                        flattened_data = flatten_json(i)
+                        row_flat_events_dict = flattened_data
+
+                        # create dict of useful info to append to results df
+                        append_dict = {
+                            'game_id' : row_game_id,
+                            'game_type' : row_game_type,
+                            'weather' : row_weather,
+                            'year' : row_year,
+                            'season_type' : row_season_type,
+                            'week' : row_week,
+                            'period_type' : row_period_type,
+                            'period_id' : row_period_id,
+                            'period_number' : row_period_number,
+                            'event_id' : row_event_id,
+                            'play_type' : row_play_type,
+                            'play_type' : row_play_type,
+                            'play_type' : row_play_type,
+                            'play_type' : row_play_type,
+                            'clock': row_clock,
+                            'home_points' : row_home_points, 
+                            'away_points' : row_away_points,
+                            'play_description' : row_play_description,
+                            'flat_events_dict' : row_flat_events_dict,
+                        }
+
+                        print(append_dict)
+
+                    except:
+
+                        print(row_period_id)
+
+
+                    # convert to pandas df and append to exisitng results_df
+                    new_row_df = pd.DataFrame([append_dict])
+
+                    print(results_df.columns)
+
+                    print(new_row_df.columns)
+
+                    results_df = pd.concat([results_df, new_row_df], ignore_index=True)
                         
-
-
-    # # iterate through the games field in each week
-    # for j in item['summary']:
-        
-    #     row_year = j['season']['year']
-    #     row_season_type = j['season']['type']
-
-    #     # create dict of useful info to append to results df
-    #     append_dict = {
-    #         'year': row_year,
-    #         'season_type': row_season_type,
-    #         'week': row_weeks,
-    #         'game_id': row_game_id,
-    #         'date': row_game_date,
-    #         'home_team': row_home_team,
-    #         'away_team': row_away_team,
-    #         'conference_game': row_conference_game,
-    #         'venue': row_venue,
-    #         'roof_type': row_roof_type,
-    #         'weather': row_weather,
-    #         'wind_speed': row_wind_speed,
-    #         'temp': row_temp,
-    #         'home_points': row_home_points,
-    #         'away_points': row_away_points
-
-    #     }
-
-    #     # convert to pandas df and append to exisitng results_df
-    #     new_row_df = pd.DataFrame([append_dict])
-    #     results_df = pd.concat([results_df, new_row_df], ignore_index=True)
-
-        # print(row_game_id)
-        # print(row_game_type)
-        # print(row_weather)
-        # print(row_year)
-        # print(row_week)
-        # print(row_period_type)
-        # print(row_period_id)
-        # print(row_period_number)
+print(results_df.groupby('play_type')['play_type'].count())
